a1ca09216872f46520b34ec0d8480c2421bb2c57
kle-pra <klemen.pravdic@arnes.si>
2017-03-30 08:52:43 +0200

    /

--- movie-listings-app/js/main.js
+++ movie-listings-app/js/main.js
@@ -52,24 +52,36 @@ function getMovie() {
     axios.get(API + 'i=' + movieId)
         .then((response) => {
             let movie = response.data;
-          
+            console.log(movie);
+
             output += `
+            <div class="row">
                 <div class="col-md-4">
-                    <div class="well text-center">
-                        <img src="${movie.Poster}"/>
-                        <div class="alert alert-success" role="alert">${movie.Actors}</div>
-                        <div class="alert alert-info" role="alert">${movie.Awards}</div>
-                        <div class="alert alert-warning" role="alert">${movie.Director}</div>
-                        <div class="alert alert-danger" role="alert">${movie.Genre}</div>
+                    <div class="well">
+                        <img src="${movie.Poster} class="thumbnail"/>
                     </div>
                 </div>
                 <div class="col-md-8">
 
                     <div class="well text-center">
-                        <h5>${movie.Title}</h5>
-                        <div class="alert alert-success" role="alert">${movie.Actors}</div>
+                        <h3>${movie.Title} <span class="badge">IMDB Rating: ${movie.imdbRating}</span></h3>
+                        <ul class="list-group">
+                            <li class="list-group-item">${movie.Year}  (${movie.Type})</li>
+                            <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+                            <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+                            <li class="list-group-item"><strong>Language:</strong> ${movie.Language}</li>
+                        </ul>
                     </div>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="well text-center">
+                         <a class="btn btn-default" href="/">Back to search</a>
+                         <a class="btn btn-default" href="http://www.imdb.com/title/${movieId}">Imdb link</a>
+                    </div>
+                </div>
+            </div>
             `;
 
             $('#movie').html(output);
