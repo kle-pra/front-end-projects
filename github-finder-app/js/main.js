20e83f3143915a638c6ba50cc16f49e7fedb8d14
klepra <klemen.pravdic@gmail.com>
2017-03-31 21:00:09 +0200

    -

--- github-finder-app/js/main.js
+++ github-finder-app/js/main.js
@@ -5,57 +5,75 @@ $('document').ready(() => {
 });
 
 const API_URL = 'https://api.github.com/users';
+const CLIENT_ID = '4ff8e2794e8670bb83f7';
+const CLIENT_SECRET = 'e3b8042fb90ef5161902998f7c09daa7a57cdba5';
 
 function displayResult() {
     let searchResultOutput = $('#searchResultOutput');
     let inputVal = $('#searchInput').val();
     let repoOutput = '';
     let output = '';
-    
-    $.get(API_URL + "/" + inputVal,
+
+    $.get(API_URL + "/" + inputVal, {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET
+
+    },
         (data, textStatus, jqXHR) => {
-            
+
             $.get(data.repos_url,
+                {
+                    "client_id": CLIENT_ID,
+                    "client_secret": CLIENT_SECRET
+
+                },
                 (repos, textStatus, jqXHR) => {
                     console.log(repos);
-                    $.each(repos, function (indexInArray, valueOfElement) {
+                    $.each(repos, function (indexInArray, repo) {
                         repoOutput +=
                             `
-                        <div class='row'>
-                            <div class="col-sm-12">
-                                <div class='well'>                    
-                                    <ul class="list-group">
-                                        <li class="list-group-item">${valueOfElement.name}</li>
-                                        <li class="list-group-item">${valueOfElement.description}</li>
-                                        <li class="list-group-item">${valueOfElement.language}</li>
-                                        <li class="list-group-item">${valueOfElement.public_repos}</li>
-                                    </ul>                   
+                            <div class="col-sm-6 col-md-4">
+                                <div class="thumbnail repo">
+                                    <div class="caption">
+                                        <h3>${repo.name} <span class="label label-info">${repo.language}</span></h3>
+                                        <p class="description">${repo.description}</p>
+                                        <p>
+                                            <a href="${repo.url}" class="btn btn-primary" role="button">Details</a>
+                                            <a href="${repo.html_url}" class="btn btn-primary" role="button">Link</a>
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>                      
-                        </div>`;
+                            </div>
+                            `;
                     });
                     $('#repos').html(repoOutput);
                 }
             );
+
             console.log(data);
-            output +=
-                `<h2>${data.login}</h2>
-                <div class='row'>
-                    <div class="col-md-4">
-                         <a href="#" class="thumbnail">
-                            <img src="https://avatars0.githubusercontent.com/u/2387907?v=3" alt="...">
-                        </a>
+            output += `
+                <div class='container'>
+                    <div class='row'>
+                        <div class="col-md-3">                                         
+                        </div> 
+                        <div class="col-md-6">
+                            <div class="thumbnail">                       
+                                <img id="avatar" src=${data.avatar_url} alt="..."> 
+                                <h2>${data.login}</h2>
+                                <ul class="list-group">
+                                    <li class="list-group-item">Name: ${data.name}</li>
+                                    <li class="list-group-item">URL ${data.url}</li>
+                                    <li class="list-group-item">Num. of repos: ${data.public_repos}</li>
+                                </ul>  
+                            </div>
+                        </div>
+                        <div class="col-md-3">                                         
+                        </div> 
                     </div>
-                    <div class="col-md-8">                   
-                        <ul class="list-group">
-                            <li class="list-group-item">Name: ${data.name}</li>
-                            <li class="list-group-item">URL ${data.url}</li>
-                            <li class="list-group-item">Num. of repos: ${data.public_repos}</li>
-                        </ul>                   
-                    </div> 
-                </div>
-                <h3>Repos</h3>
-                <div id='repos' class='well'></div>`;
+                    <h3>Repos</h3>
+                    <div id='repos' class='row'>
+                    </row>
+                </div>`;
             searchResultOutput.html(output);
         }
     );
