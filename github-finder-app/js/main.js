418573e6af33481d3b7ccea5764e693739ab9686
kle-pra <klemen.pravdic@arnes.si>
2017-03-31 14:28:29 +0200

    -

--- github-finder-app/js/main.js
+++ github-finder-app/js/main.js
@@ -1,24 +1,51 @@
 $('document').ready(() => {
-
-    const API_URL = 'https://api.github.com/search/users';
-
-    let searchResultOutput = $('#searchResultOutput');
-
     $('#searchInput').on("keyup", () => {
-
-        let inputVal = $('#searchInput').val();
-
-        $.get(API_URL + "?q=" + inputVal + "&in=login",
-            (data, textStatus, jqXHR) => {
-                console.log(data);
-                searchResultOutput.html(data);
-               
-            }
-        );
-
-
-
+        displayResult();
     });
-
-
 });
+
+const API_URL = 'https://api.github.com/users';
+
+function displayResult() {
+    let searchResultOutput = $('#searchResultOutput');
+    let inputVal = $('#searchInput').val();
+    let output = '';
+    
+    $.get(API_URL + "/" + inputVal,
+        (data, textStatus, jqXHR) => {
+             output = '';
+            $.get(data.repos_url,
+                (repos, textStatus, jqXHR) => {
+                    console.log(repos);
+                    $.each(repos, function (indexInArray, valueOfElement) {
+                        output +=
+                            `
+                        <div "well">                    
+                            <ul class="list-group">
+                                <li class="list-group-item">${valueOfElement.name}</li>
+                                <li class="list-group-item">${valueOfElement.description}</li>
+                                <li class="list-group-item">${valueOfElement.language}</li>
+                                <li class="list-group-item">${valueOfElement.public_repos}</li>
+                            </ul>                   
+                        </div>`;
+                    });
+                    $('#repos').html(output);
+                }
+            );
+            console.log(data);
+            output =
+                `<h2>${data.login}</h2>
+                <div "well">                    
+                    <ul class="list-group">
+                        <li class="list-group-item">${data.login}</li>
+                        <li class="list-group-item">${data.name}</li>
+                        <li class="list-group-item">${data.organizations_url}</li>
+                        <li class="list-group-item">${data.public_repos}</li>
+                        <li class="list-group-item">${data.login}</li>
+                    </ul>                   
+                </div>
+                <div id='repos' class='well'></div>`;
+            searchResultOutput.html(output);
+        }
+    );
+}
