2d78bea121e70a03bdb98d7f141c463402e83a15
klepra <klemen.pravdic@gmail.com>
2017-05-06 16:33:58 +0200

    quote app finish

--- random-quote-finder/main.js
+++ random-quote-finder/main.js
@@ -1,28 +1,18 @@
 $(document).ready(() => {
     loadRandomQuote();
-    $("#loadQuoteButton").on('click', () => {
-        loadRandomQuote();
-    });
-
-    $("#tweetQuoteButton").on('click', () => {
-        tweetCurrentQuote();
-    });
+    $("#loadQuoteButton").on('click', loadRandomQuote);
 });
 
 function loadRandomQuote() {
-    console.log("call API and show data");
     $.ajax({
         dataType: "json",
         url: "https://random-quote-generator.herokuapp.com/api/quotes/random",
         success: (result, status, xhr) => {
+            //setup quote text html
             $("#author").html(result.author);
             $("#quote").html(result.quote);
-
             //setup tweet url
-            $("#tweetQuoteButton").attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=test&text='
-     + encodeURIComponent('"' +  $("#quote").html() + '" ' +  $("#author").html()));
-
-
+            $("#tweetQuoteButton").attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' +  $("#quote").html() + '" ' +  $("#author").html()));
         }
     });
 }
