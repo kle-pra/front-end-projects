4d9289ec86386e7143c376cbfe575be50c46d69a
klepra <klemen.pravdic@gmail.com>
2017-05-06 23:48:13 +0200

    finished weather app

--- display-local-weather/main.js
+++ display-local-weather/main.js
@@ -1,5 +1,7 @@
 $(document).ready(() => {
     displayWeather();
+
+    $("#temperature").on("click", toggleTemperature);
 });
 
 function displayWeather() {
@@ -12,10 +14,29 @@ function displayWeather() {
                 dataType: "json",
                 url: "http://api.openweathermap.org/data/2.5/weather?q="+result.city + "&APPID=" + "2e177495936266587a8e9af03cc3eee6",
                 success: (result, status, xhr) => {
-                    $("#weatherContent").html(JSON.stringify(result));
+                    console.log(result);
+                    var tempKalvin = result.main.temp;
+                    var tempCelsius = Math.round(tempKalvin-273.15);
+                    var weather = result.weather[0].description;
+                    var location= result.name;
+                    $("#weather").html(weather);
+                    $("#location").html(location);
+                    $("#temperature").html(tempCelsius+" °C");
+                    $("#icon").attr("src", "http://openweathermap.org/img/w/"+result.weather[0].icon+".png")
+                    var currentTemp = result.main.temp;  
                 }
             });
         }
     });
 
+}
+
+function toggleTemperature(){
+    if($("#temperature").html().indexOf("°C")>0){
+        var tempCelsius=parseInt($("#temperature").html());
+        $("#temperature").html(Math.round(tempCelsius*9/5+32)+ " F");
+    } else {
+         var tempFahrenheit=parseInt($("#temperature").html());
+        $("#temperature").html(Math.round((tempFahrenheit-32)*5/9)+ " °C");
+    }
 }
