2d43b8039b55920e93037f91954a3c2b397c3a83
klepra <klemen.pravdic@gmail.com>
2017-05-06 18:46:44 +0200

    weather app ajax call working

--- display-local-weather/main.js
+++ display-local-weather/main.js
@@ -4,18 +4,18 @@ $(document).ready(() => {
 
 function displayWeather() {
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-
-            $.ajax({
+    $.ajax({
+        dataType: "json",
+        url: "http://ip-api.com/json",
+        success: (result, status, xhr) => {
+             $.ajax({
                 dataType: "json",
-                url: "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude+"&APPID="+"2e177495936266587a8e9af03cc3eee6",
+                url: "http://api.openweathermap.org/data/2.5/weather?q="+result.city + "&APPID=" + "2e177495936266587a8e9af03cc3eee6",
                 success: (result, status, xhr) => {
                     $("#weatherContent").html(JSON.stringify(result));
                 }
             });
-        });
+        }
+    });
 
-
-    }
 }
