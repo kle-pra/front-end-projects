c2fb2a7eb1268aae86b5d48b5f094b67fdb20dfb
klepra <klemen.pravdic@gmail.com>
2017-05-06 18:23:11 +0200

    weather app API call

--- display-local-weather/main.js
+++ display-local-weather/main.js
@@ -1,1 +1,21 @@
+$(document).ready(() => {
+    displayWeather();
+});
 
+function displayWeather() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+
+            $.ajax({
+                dataType: "json",
+                url: "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude+"&APPID="+"2e177495936266587a8e9af03cc3eee6",
+                success: (result, status, xhr) => {
+                    $("#weatherContent").html(JSON.stringify(result));
+                }
+            });
+        });
+
+
+    }
+}
