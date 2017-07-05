11f620e28e5291c9a0b3b05cc902acc7ddd969b1
klepra <klemen.pravdic@gmail.com>
2017-07-05 18:32:56 +0200

    google maps

---
+++ google-maps-api/README.md
@@ -0,0 +1,2 @@
+Trying out google maps with js, following this nice tutorial:
+https://www.youtube.com/watch?v=Zxf1mnP5zcw
---
+++ twitch-app/main.js
@@ -0,0 +1,2 @@
+var channels =
+["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
